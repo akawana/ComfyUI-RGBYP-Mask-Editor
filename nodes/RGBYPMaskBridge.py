0531701a977fa2fb924a243d507b4dfbe1e1ab9c
akawana <pishite@protonmail.com>
2026-02-09 23:45:46 +0800

    firt run fix

--- nodes/RGBYPMaskBridge.py
+++ nodes/RGBYPMaskBridge.py
@@ -144,10 +144,18 @@ class RGBYPMaskBridge:
 
         _, inputHeight, inputWidth, _ = image.shape
 
-        isInputImageChanged, new_fp = is_input_image_changed_variantA(
-            self._get_last_input_fp(), image
-        )
+        last_fp = self._get_last_input_fp()
+        isInputImageChanged, new_fp = is_input_image_changed_variantA(last_fp, image)
         self._set_last_input_fp(new_fp)
+
+        # first run after server start: don't trigger the "input changed" branch
+        if last_fp is None:
+            isInputImageChanged = False
+
+        # isInputImageChanged, new_fp = is_input_image_changed_variantA(
+        #     self._get_last_input_fp(), image
+        # )
+        # self._set_last_input_fp(new_fp)
 
         output_image = image
         rgbypOutputMask = _make_black_64(device=image.device, dtype=image.dtype)
